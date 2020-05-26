10a9282719592471da7b2aba3a8ab7010c89b9c9
Isabella Panagrosso <isabellapanagrosso15@gmail.com>
2020-05-26 14:33:04 +0000

    Submitting second exercise

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,12 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+
+@store = Store.find_by(id: 1)
+@store1 = Store.find_by(id: 2)
+
+@store.name = "Something"
+@store.save
+
+stores = Store.all
+print stores
