fd4192857afd99a89bcff930132ff001c38d726d
Isabella Panagrosso <isabellapanagrosso15@gmail.com>
2020-05-26 15:57:39 +0000

    Added some data to employees database

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,16 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+
+@store1.employees.create(first_name:"Bella", last_name:"Pana", hourly_rate: 100)
+@store1.employees.create(first_name:"Sam", last_name:"Pngrs", hourly_rate: 100)
+@store1.employees.create(first_name:"Caryn", last_name:"Kaftal", hourly_rate: 200)
+@store1.employees.create(first_name:"Phil", last_name:"Grosso", hourly_rate: 200)
+
+@store2.employees.create(first_name:"Ari", last_name:"Pedre", hourly_rate: 500)
+@store2.employees.create(first_name:"Stefanie", last_name:"Ablo", hourly_rate: 500)
+@store2.employees.create(first_name:"Elvi", last_name:"Ralli", hourly_rate: 500)
+
+
+
+
